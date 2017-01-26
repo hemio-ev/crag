8b7eea7f86e56b1a6dded7ca578174034cc8f6dc
Sophie Herold <sophie@hemio.de>
2017-01-26 18:20:02 +0100

    Implements FlatJWS

--- src/Main.hs
+++ src/Main.hs
@@ -6,32 +6,64 @@ import Crypto.JOSE.JWS
 import Crypto.JOSE.Types
 import Crypto.JOSE.JWK
 import Crypto.JOSE.Error
+import Crypto.JOSE
 
 import Data.Maybe
 import Control.Monad.Except
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as L
+import Data.Yaml.Pretty
+
+import Data.Aeson
+import Data.Monoid
+import GHC.Exts (fromList)
 
 
 main :: IO ()
 main = do 
-  let jwsContent = newJWS "Content Text"
-  let header = newJWSHeader (Unprotected, RS256)
+  let jwsContent = newJWS "{\
+\       \"agreement\": \"https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf\",\
+\       \"resource\": \"new-reg\",\
+\       \"contact\": [\
+\         \"mailto:sophie_test_1@hemio.de\"\
+\       ]\
+\     }"
 
-  jwk1 <- genJWK (RSAGenParam 512)
-  jwk2 <- genJWK (RSAGenParam 512)
+  key1 <- generateKey
+  key2 <- generateKey
   
-  signed <- runExceptT $ signJWS jwsContent header jwk1
+  jsonNice key1
   
-  let x = case signed of
-            Left (e :: Error) -> error $ show $ e
-            Right jwsSign -> jwsSign
-            
-  putStrLn $ show x
-  
-  (show <$> very jwk1 x) >>= putStrLn
-  (show <$> very jwk2 x) >>= putStrLn
-  
-  putStrLn "By."
+  signed <- signWith jwsContent key1
+  L.writeFile "post.json" (encode $ toFlatJSON signed)
 
+  return ()
+
+generateKey :: IO KeyMaterial
+generateKey = genKeyMaterial (ECGenParam P_256)
+
+toFlatJSON (JWS p (s:[])) = Object $
+    toObject s <>
+    fromList ["payload" .= p]
+toFlatJSON (JWS _ _) = undefined
+
+toObject :: ToJSON a => a -> Object
+toObject a = case toJSON a of
+  Object o -> o
+  _        -> error "toObject: value isn't an Object"
+
+jsonNice = B.putStrLn . encodePretty defConfig
+
+signWith jwsContent keyMat = do
+      signed <- runExceptT $ signJWS jwsContent header jwk
+  
+      return $ case signed of
+                Left (e :: Error) -> error $ show $ e
+                Right jwsSign -> jwsSign
+  where
+    header = (newJWSHeader (Unprotected, ES256)) {
+      _jwsHeaderJwk = Just $ HeaderParam Unprotected jwk }
+    jwk = fromKeyMaterial keyMat
 
 very x y = do
   y <- runExceptT $ verifyJWS defaultValidationSettings x y
